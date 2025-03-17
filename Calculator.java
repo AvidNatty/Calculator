bb3d644998134ef3ee82975820bfca64aca3cd29
Avidnatty <avidnatty@gmail.com>
2025-03-17 18:16:16 +0200

    1st commit

--- Calculator.java
+++ Calculator.java
@@ -1,13 +1,20 @@
 public class Calculator{
-static int num1=10;
-static int num2=2;
-static int answer;
-String name;
-String car;
-public Calculator(){
+private static int num1=10;
+private static int num2=2;
+private static int answer;
+private String name;
+private String car;
+Calculator(){
 }
-public int add(){
-answer=num1+num2;
+Calculator(String name, String car){
+this.name=name;
+this.car=car;
+}
+public String toString(){ 
+return "Calculator{" + "name=" + name + ",car=" + car+'}';
+} 
+public int add(){ 
+answer=num1+num2; 
 return answer;
 }
 public int subt(){
@@ -21,10 +28,6 @@ return answer;
 public static int divide(){
 answer=num1/num2;
 return answer;
-}
-public Calculator(String name, String car){
-this.name=name;
-this.car=car;
 }
 public static void main(String[]args){
 Calculator c=new Calculator("Quirine", "Toyota");

53187d87987c44298a79559e12d9134f52de472f
Avidnatty <avidnatty@gmail.com>
2025-03-17 14:19:15 +0200

    1st commit

--- Calculator.java
+++ Calculator.java
@@ -2,9 +2,11 @@ public class Calculator{
 static int num1=10;
 static int num2=2;
 static int answer;
+String name;
+String car;
 public Calculator(){
 }
-public static int add(){
+public int add(){
 answer=num1+num2;
 return answer;
 }
@@ -16,15 +18,24 @@ public int mult(){
 answer=num1*num2;
 return answer;
 }
-public int divide(){
+public static int divide(){
 answer=num1/num2;
 return answer;
 }
-public Calculator(int num1, int num2){
+public Calculator(String name, String car){
+this.name=name;
+this.car=car;
 }
 public static void main(String[]args){
-Calculator c=new Calculator(10,2);
-add();
+Calculator c=new Calculator("Quirine", "Toyota");
 System.out.println(c);
+c.add();
+System.out.println("value of num1+num2 =" +answer);
+c.subt();
+System.out.println("value of num1-num2 =" +answer);
+c.mult();
+System.out.println("value of num1*num2 =" +answer);
+Calculator.divide();
+System.out.println("value of num1/num2 =" +answer);
 }
 }

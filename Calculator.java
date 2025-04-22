80c6a963a1120e89a5787b66ec43920e0565486a
Avidnatty <avidnatty@gmail.com>
2025-04-22 07:49:55 +0200

    Variables made private and constructors added.

--- Calculator.java
+++ Calculator.java
@@ -1,44 +1,66 @@
+package calc;
+
 public class Calculator{
-private static int num1=10;
-private static int num2=2;
+private static int num1 = 10;
+private static int num2 = 2;
 private static int answer;
-private String name;
-private String car;
-Calculator(){
+
+public Calculator() {
 }
-Calculator(String name, String car){
-this.name=name;
-this.car=car;
+public Calculator(int num1, int num2, int answer) {
+    this.num1 = num1;
+    this.num2 = num2;
+    this.answer = answer;
 }
-public String toString(){ 
-return "Calculator{" + "name=" + name + ",car=" + car+'}';
-} 
-public int add(){ 
-answer=num1+num2; 
-return answer;
+
+public int getNum1() {
+    return num1;
 }
-public int subt(){
-answer=num1-num2;
-return answer;
+public int getNum2() {
+    return num2;
 }
-public int mult(){
-answer=num1*num2;
-return answer;
+public static int getAnswer() {
+    return answer;
 }
-public static int divide(){
-answer=num1/num2;
-return answer;
+
+public void setNum1(int num1) {
+    this.num1 = num1;
 }
-public static void main(String[]args){
-Calculator c=new Calculator("Quirine", "Toyota");
-System.out.println(c);
-c.add();
-System.out.println("value of num1+num2 =" +answer);
-c.subt();
-System.out.println("value of num1-num2 =" +answer);
-c.mult();
-System.out.println("value of num1*num2 =" +answer);
-Calculator.divide();
-System.out.println("value of num1/num2 =" +answer);
+public void setNum2(int num2) {
+    this.num2 = num2;
 }
+public void setAnswer(int answer) {
+    this.answer = answer;
 }
+
+public int add () {
+    answer = num1 + num2;
+    return answer;
+}
+public int subt () {
+    answer = num1 - num2;
+    return answer;
+}
+public int mult () {
+    answer = num1 * num2;
+    return answer;
+}
+public int divide() {
+    answer = num1 / num2;
+    return answer;
+}
+
+public static void main(String[] args) {
+    Calculator c=new Calculator(10, 2, 10&2);
+    System.out.println(c);
+    c.add();
+    System.out.println("value of num1+num2 =" +getAnswer());
+    c.subt();
+    System.out.println("value of num1-num2 =" +getAnswer());
+    c.mult();
+    System.out.println("value of num1*num2 =" +getAnswer());
+    c.divide();
+    System.out.println("value of num1/num2 =" +getAnswer());
+}
+      }
+
